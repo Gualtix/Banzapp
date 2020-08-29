5333de2183b0f08212f0f7a5ba8f620a60f34859
Jose Axpuac <jose.axpuac@gmail.com>
2020-08-28 21:51:33 -0600

    new API schema

---
+++ banzapi/banzapi/Controllers/CursoController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using banzapi.DAL;
+using Newtonsoft.Json;
+
+namespace banzapi.Controllers
+{
+    public class CursoController : ApiController
+    {
+        [HttpGet] // GET: api/Curso
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+                    List<CURSO> listadoCursos = db.CURSO.Select(s => s).ToList();
+                    return Ok(listadoCursos);
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+            
+        }
+
+        [HttpGet] // GET: api/Curso/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+                    CURSO cursoBuscado = db.CURSO.FirstOrDefault(s => s.codigo == id);
+                    if (cursoBuscado != null)
+                    {
+                        return Ok(cursoBuscado);
+                    }
+
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPost] // POST: api/Curso
+        public IHttpActionResult Post([FromBody]string value)
+        {
+            try
+            {
+                CURSO nuevoCurso = JsonConvert.DeserializeObject<CURSO>(value);
+
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+                    CURSO cursoExistente = db.CURSO.FirstOrDefault(c => c.codigo == nuevoCurso.codigo);
+                    if (cursoExistente != null)
+                    {
+                        cursoExistente.creditos = nuevoCurso.creditos;
+                        cursoExistente.nombre = nuevoCurso.nombre;
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.CURSO.Add(nuevoCurso);
+                        db.SaveChanges();
+                    }
+
+                    return Ok();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPut]// PUT: api/Curso/5
+        public IHttpActionResult Put([FromBody]string value)
+        {
+            try
+            {
+                CURSO nuevoCurso = JsonConvert.DeserializeObject<CURSO>(value);
+
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+
+                    CURSO cursoExistente = db.CURSO.FirstOrDefault(c => c.codigo == nuevoCurso.codigo);
+                    if (cursoExistente != null)
+                    {
+                        cursoExistente.creditos = nuevoCurso.creditos;
+                        cursoExistente.nombre = nuevoCurso.nombre;
+                        db.SaveChanges();
+                        return Ok();
+                    }
+                        
+                    return NotFound();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpDelete] // DELETE: api/Curso/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+                    CURSO cursoExistente = db.CURSO.FirstOrDefault(c => c.codigo == id);
+                    if (cursoExistente != null)
+                    {
+                        db.CURSO.Remove(cursoExistente);
+                        db.SaveChanges();
+                        return Ok();
+                    }
+
+                    return NotFound();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+    }
+}
---
+++ banzapi/banzapi/Controllers/EscuelaController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using banzapi.DAL;
+using Newtonsoft.Json;
+
+namespace banzapi.Controllers
+{
+    public class EscuelaController : ApiController
+    {
+        [HttpGet] // GET: api/ESCUELA
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+                    List<ESCUELA> listadoEscuelas = db.ESCUELA.Select(s => s).ToList();
+                    return Ok(listadoEscuelas);
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+        }
+
+        [HttpGet] // GET: api/ESCUELA/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+                    ESCUELA ESCUELAsearch = db.ESCUELA.FirstOrDefault(s => s.id == id);
+                    if (ESCUELAsearch != null)
+                    {
+                        return Ok(ESCUELAsearch);
+                    }
+
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPost] // POST: api/ESCUELA
+        public IHttpActionResult Post([FromBody] string value)
+        {
+            try
+            {
+                ESCUELA newESCUELA = JsonConvert.DeserializeObject<ESCUELA>(value);
+
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+                    ESCUELA ESCUELAExistente = db.ESCUELA.FirstOrDefault(c => c.id == newESCUELA.id);
+                    if (ESCUELAExistente != null)
+                    {
+                        ESCUELAExistente.nombre = newESCUELA.nombre;
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.ESCUELA.Add(newESCUELA);
+                        db.SaveChanges();
+                    }
+
+                    return Ok();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPut]// PUT: api/ESCUELA/5
+        public IHttpActionResult Put([FromBody] string value)
+        {
+            try
+            {
+                ESCUELA newESCUELA = JsonConvert.DeserializeObject<ESCUELA>(value);
+
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+
+                    ESCUELA ESCUELAExistente = db.ESCUELA.FirstOrDefault(c => c.id == newESCUELA.id);
+                    if (ESCUELAExistente != null)
+                    {
+                        ESCUELAExistente.nombre = newESCUELA.nombre;
+
+                        db.SaveChanges();
+                        return Ok();
+                    }
+
+                    return NotFound();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpDelete]// DELETE: api/ESCUELA/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+
+                using (BanzdbEntities db = new BanzdbEntities())
+                {
+
+                    ESCUELA ESCUELAExistente = db.ESCUELA.FirstOrDefault(c => c.id == id);
+                    if (ESCUELAExistente != null)
+                    {
+                        db.ESCUELA.Remove(ESCUELAExistente);
+                        db.SaveChanges();
+                        return Ok();
+                    }
+
+                    return NotFound();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+
+}
