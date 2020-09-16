3d52fb276761396f53c9ca0b61eb1db247da4120
ynabe <2636889760101@ingenieria.usac.edu.gt>
2020-09-16 00:19:53 -0600

    se creo el test para el registro de estudiantes

--- banzapi/banzapi.Tests/UnitTest1.cs
+++ banzapi/banzapi.Tests/UnitTest1.cs
@@ -1,5 +1,15 @@
 ﻿using System;
+using banzapi.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using banzapi.DAL;
+using banzapi.Models;
+using Moq;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace banzapi.Tests
 {
@@ -9,6 +19,25 @@ namespace banzapi.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var context = new BanzdbEntities();
+            var mockRepository = new Mock<StudentRepository>(context);
+            var controller = new AdminController(mockRepository.Object);
+
+            var model = new ESTUDIANTE()
+            {
+                carnet = 2014,
+                nombre = "aldo perez",
+                email = "abc@gmail.com",
+                passw = "1234",
+                fk_escuela = 1
+            };
+
+            IHttpActionResult actionResult = controller.PostResgistro(model);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<ESTUDIANTE>;
+
+            //Assert.IsNotNull(createdResult);
+            //Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            //Assert.AreEqual(model.email, createdResult.RouteValues["email"]);
         }
     }
 }
