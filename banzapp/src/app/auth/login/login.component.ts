72024b2291c1429b901eadf1d4b3c0b9ae70d582
Ramon Patzan <ramon.os.pc@gmail.com>
2020-09-19 18:56:10 -0600

    Se agrego la vista grafica del login y sus estilos

--- banzapp/src/app/auth/login/login.component.ts
+++ banzapp/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   login(){
 
   }
