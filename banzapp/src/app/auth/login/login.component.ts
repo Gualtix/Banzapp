b2db5d76552b93f976f9c30f3226546b3d323886
Ramon Patzan <ramon.os.pc@gmail.com>
2020-09-25 18:22:53 -0600

    Se agregaron funcionalidades al componente del login

--- banzapp/src/app/auth/login/login.component.ts
+++ banzapp/src/app/auth/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DummyService } from 'src/app/services/dummy.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -10,23 +11,46 @@ import { DummyService } from 'src/app/services/dummy.service';
 })
 export class LoginComponent implements OnInit {
   
-  public formSubmitted = false;
+  public formulario: FormGroup;
 
-  public loginForm = this.fb.group({
-    usuario: ['' , [ Validators.required] ],
-    password: ['', Validators.required ]
-  });
-
+  
   constructor(
     private router: Router,
-    private fb: FormBuilder,
     private usuarioService: DummyService
   ) { }
 
   ngOnInit(): void {
+    this.formulario = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
   }
 
   login(){
-    
+    if(this.formulario.invalid){
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'Error'
+      });
+      return false;
+    }
+
+    this.usuarioService.login(this.formulario.value.userName,this.formulario.value.password).subscribe(
+      (data: boolean)=>{  
+        if(data === true){
+          this.router.navigate(['/dashboard']);
+          return true;
+        }
+      },
+      err=>{
+        Swal.fire({
+          icon: 'error',
+          title:'Error de credenciales',
+          text:'Error'
+        });
+        return false;
+      }
+    );
   }
 }
--- banzapp/src/app/services/dummy.service.ts
+++ banzapp/src/app/services/dummy.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders,HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,16 @@ export class DummyService {
    private SERVER_URL = "https://banzapp.azurewebsites.net/api/PensumSemestre/";
    private SERVER_PRE =  "https://banzapp.azurewebsites.net/api/Pensum/"
 
+  public id: string;
+  public carnet : string;
+  public nombre: string;
+  public token: string;
     
    constructor(
       private httpClient: HttpClient
-    ) { }
+    ) { 
+      this.cargarStorage();
+    }
 
   public getPensum(pensum:string, semestre:string) {
 
@@ -41,7 +49,62 @@ export class DummyService {
 
   }
 
-  public login(){
+  public login(Username: string, Password: string){
+    const user ={
+      usuario: Username,
+      password: Password
+    }
+
+    const url = '';
+    return this.httpClient.post(url,user).pipe(map((resp:any)=>{
+      if(resp.ok === false){
+        Swal.fire({
+          icon:'error',
+          title:'Error',
+          text:'Error de credenciales'
+        });
+        return false;
+      }else{
+         this.saveStorage(
+           resp.carnet,
+           resp.token,
+           resp.nombre,
+           resp.id);
+
+           return true;
+      }
+    })
+    );
     
+  }
+
+  public saveStorage(carnet, token, nombre,id){
+    localStorage.setItem('carnet',carnet);
+    localStorage.setItem('token', token);
+    localStorage.setItem('nombre', nombre);
+    localStorage.setItem('id',id);    
+  }
+
+  public cargarStorage(){
+    if ( localStorage.getItem('token') ){
+      this.token = localStorage.getItem('token');
+      this.id = localStorage.getItem('id');
+      this.nombre = localStorage.getItem('nombre');
+      this.carnet = localStorage.getItem('carnet');
+    }else{
+      this.token = '';
+      this.id = '';
+      this.nombre = '';
+      this.carnet = '';
+    }
+  }
+
+  public estaLogueado(){
+    this.cargarStorage();
+    if (this.token.length > 10){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
